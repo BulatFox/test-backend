e3180ab9260db127768cc57f0e822bfc08068dfa
BulatFox <fox.brot.04@gmail.com>
2024-10-10 21:50:18 +0300

    Update V1__Budget.sql

--- src/main/resources/db/migration/V1__Budget.sql
+++ src/main/resources/db/migration/V1__Budget.sql
@@ -1,8 +1,9 @@
-create table budget
+update budget set type = 'Расход' where type = 'Комиссия';
+create table author
 (
-    id     serial primary key,
-    year   int  not null,
-    month  int  not null,
-    amount int  not null,
-    type   text not null
+    id serial primary key,
+    fio varchar(256) not null,
+    creation_date timestamp not null
 );
+alter table budget add column author_id integer;
+alter table budget add constraint FK_author_id foreign key (author_id) references author (id);
